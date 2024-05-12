6d0bff5595c2aecf61605ddefc6e621c94cbbdc8
￴￴ <>
2024-05-13 02:08:08 +0700

    up

---
+++ package/github_sponsor_scheme/bin/github_sponsor_scheme.dart
@@ -0,0 +1,5 @@
+
+void main(List<String> args) async{
+  
+  
+}
---
+++ package/github_sponsor_scheme/lib/schemes/respond_schemes.dart
@@ -0,0 +1,23 @@
+List<Map> respond_schemes = [
+  {
+    "@type": "sponsor",
+    "status": "success",
+    "sponsors": {
+      "@type": "sponsorData",
+      "current": [
+        {
+          "@type": "sponsorAccountData",
+          "username": "",
+          "avatar": "",
+        },
+      ],
+      "past": [
+        {
+          "@type": "sponsorAccountData",
+          "username": "",
+          "avatar": "",
+        },
+      ],
+    },
+  },
+];
